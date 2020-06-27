7bf051d6fa39903ff60a05d0f7306c882e7676fb
Ali Hassan <Ali.Hassan@quest.com>
2020-06-27 10:57:59 -0400

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+const forecast = (long, lat, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=86925dafd37d22e06619468c708cd851&query=${encodeURIComponent(long)},${encodeURIComponent(lat)}&units=m`;
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined);
+        } else if (body.error) {
+            callback('Unable to find location', undefined);
+        } else {
+            const { temperature, precip, weather_descriptions } = body.current;
+            callback(undefined, `${weather_descriptions[0]}. It is currently ${temperature} degrees out. There is a ${precip}% chance of rain.`);
+        }
+    });
+}
+
+module.exports = forecast;
