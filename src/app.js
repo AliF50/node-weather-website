3cb21c67562e1a6c7478feb44754937c3762332e
Ali Hassan <Ali.Hassan@quest.com>
2020-06-27 12:01:37 -0400

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -75,6 +76,6 @@ app.get('*', (req, res) => {
     res.render('404', { title: '404', name: 'Andrew Mead', errorMessage: 'Page not found.' })
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`);
 });
